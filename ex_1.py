85fc843307aba47f4a1c4c592f68891ae64d7b90
Joaquin Barbeito <84950995+Soy-el-final@users.noreply.github.com>
2022-07-30 21:10:20 -0500

    Add files via upload

---
+++ ex_1.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+
+Nombre = 'Joaquin'
+Apellido = 'Barbeito'
+
+saludo = 'Estoy empezando el curso de python'
+deseo = 'Espero aprender mucho'
+
+Hola_soy = Nombre + " " + Apellido
+Presentacion  = saludo + " , " + deseo
+
+print('Hola_soy: ', Hola_soy)
+print('Presentacion:',Presentacion)
